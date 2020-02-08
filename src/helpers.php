19c3959c5c5058f29b1da625d4374f9f3faa7f3c
SkyLiner <alexandreluisbarreto@gmail.com>
2020-02-08 19:33:14 +0000

    Adds a new helper function to render the views

---
+++ src/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+use Slim\Http\Response;
+
+if (!function_exists('view')) {
+    /**
+     * Renders the view selected by the user
+     *
+     * @param Slim\Http\Response $response
+     * @param string $twig_path
+     *
+     * @return Response
+     */
+    function view(Response $response, string $twig_path): Response
+    {
+        global $app;
+        return $app->getContainer()->view->render($response, $twig_path);
+    }
+}
