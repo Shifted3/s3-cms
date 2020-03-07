bafbc89fdf420c4703a699af4d05ab7944f67ef5
SkyLiner <alexandreluisbarreto@gmail.com>
2020-03-07 14:02:34 +0000

    Adds helper functions to the composer autoload

--- src/helpers.php
+++ src/helpers.php
@@ -2,51 +2,63 @@
 
 use Slim\Http\Response;
 
+/**
+ * Shifted 3 Framework
+ *
+ * File Description:
+ * These are function helpers for the framework
+ *
+ * WARNING::  Unless you know what you are doing; we do not recommend changing these settings.
+ * Buyer beware; you have been warned!
+ *
+ */
+
 if (!function_exists('view')) {
     /**
      * Renders the view selected by the user
      *
      * @param Slim\Http\Response $response
-     * @param string $twig_path Can use / or . as directory path
+     * @param string $view_path Can use / or . as directory path
+     * @param array $data
      *
      * @return Response
      */
-    function view(Response $response, string $twig_path, array $data = []): Response
+    function view(Response $response, string $view_path, array $data = []): Response
     {
         global $container;
 
-        if (!str_contains($twig_path, '/')) {
-            $twig_path = explode('.', $twig_path);
+        if (!str_contains($view_path, '/')) {
+            $view_path = explode('.', $view_path);
 
-            if (!is_bool($twig_path) && is_array($twig_path)) {
+            if (!is_bool($view_path) && is_array($view_path)) {
                 $new_path = "";
-                foreach ($twig_path as $key => $value) {
-                    if (++$key != count($twig_path)) {
+                foreach ($view_path as $key => $value) {
+                    if (++$key != count($view_path)) {
                         $new_path .= $value . '/';
                     } else {
                         $new_path .= $value;
                     }
                 }
 
-                $twig_path = $new_path;
+                $view_path = $new_path;
             }
         }
 
-        if (!str_contains($twig_path, '.twig')) {
-            $twig_path = $twig_path . '.twig';
+        if (!str_contains($view_path, '.twig')) {
+            $view_path = $view_path . '.twig';
         }
 
-        return $container->view->render($response, $twig_path, $data);
+        return $container->view->render($response, $view_path, $data);
     }
 }
 
 if (!function_exists('str_contains')) {
     /**
      * Checks wether a string contains a certain value
-     * 
+     *
      * @param string $haystack The string to search in
      * @param string $needle The string to search for
-     * 
+     *
      * @return bool
      */
     function str_contains(string $haystack, string $needle): bool
